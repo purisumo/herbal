2b25ed1d212feb4b7b790a9deb773a0ebe348cb3
purisumo <124586989+purisumo@users.noreply.github.com>
2023-11-25 19:41:29 +0800

    Delete herbal/staticfiles directory

--- herbal/staticfiles/js/main.js
+++
@@ -1,7 +0,0 @@
-       let imgContainer = document.querySelector(".img-container");
- 
-setInterval(() => {
-        let last = imgContainer.firstElementChild;
-        last.remove();
-        imgContainer.appendChild(last);
-      }, 2500);
